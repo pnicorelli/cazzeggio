a5efc17916792d6720fe2e87daf584ae6aab5fc7
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2016-01-29 17:16:25 +0100

    add port

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var app = express();
+var port;
 
 app.get('/', function(req, res){
   res.send('hello world');
 });
 
-app.listen(3000);
+port = 3000;
+
+app.listen(port, function () {
+  console.log('Ready on '+port);
+});
