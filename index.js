ba8feb9ddd774f423ef7c4370e82fc3f0eabe51e
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2016-01-29 17:24:58 +0100

    first home page

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
-var port;
+var loadPage = require('./loadPage');
+
+var port, myHtml;
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  loadPage( function( err, data){
+    if( err ){
+      res.send(err);
+    } else {
+      myHtml = '<h1>'+data.title+'</h1>';
+      myHtml += data.image;
+      myHtml += '<br><br><br>';
+      myHtml += '<button onclick="location.reload();">Ricarica</button>';
+      myHtml += '<br><br><br>';
+      myHtml += 'powerd by cazzeggio';
+      res.send(myHtml);
+    }
+  })
+
 });
 
 port = process.env.PORT || 3000;
